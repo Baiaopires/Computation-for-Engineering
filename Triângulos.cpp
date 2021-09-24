12ac6991b760b3306679dbe2df0eded47b0de51c
Baiaopires <85589369+Baiaopires@users.noreply.github.com>
2021-09-23 23:19:28 -0300

    Update Triângulos.cpp

--- Triângulos.cpp
+++ Triângulos.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 
 int main(){
+    //Declaração das variáveis do tipo double
 	double x, y, z;
+	//Entrada do usuário para os respectivos valores das medidas dos lados do triângulo
 	cin >> x >> y >> z;
+	//Algoritmo que diz o tipo de triângulo formado com os parámetros "inputados"
 		if (x < y + z && y < x + z && z < x + y){
-			if (x == y && x == z ){
+			if (x == y && x == z && y == z){
 				cout << "equilatero";
 			}
 			else if ((x == y && x < z) || (x == z && x < y) || (y == z && y < x)){
@@ -17,9 +20,9 @@ int main(){
 				cout << "escaleno";
 			}
 		}
+		//Mensagem padrão mostrada caso nenhuma das condições anteriores sejam cumpridas
 		else {
 		cout << "invalido\n"; }
 
-	
 	return 0;
 }
