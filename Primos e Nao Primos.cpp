84d0d569b0953e887ddaa6552278814ade5a5ecf
Baiaopires <85589369+Baiaopires@users.noreply.github.com>
2021-09-23 23:25:24 -0300

    Update Primos e Nao Primos.cpp

--- Primos e Nao Primos.cpp
+++ Primos e Nao Primos.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 
 int main(){
+    //Declaração das variáveis, neste caso, "num" para número e "deno" para denominador
 	int num, deno;
 	
+	//Input do usuário na variável "num"
 	cin >> num;
 	
+	//Aqui, eu basicamente estabeleci que a variável "deno" guarde o valor atual de "num"
 	deno = num;
 	
+	//Nesta seção, o algoritimo vai testar se "num", primeiramente, é maior que -1, ou seja, positivo, pois "num" é do tipo int.
+	//Com isso, a variável entra no primeiro loop "while", em que o programa irá testar se "num" é diferente de zero e de 1. Caso
+	//as duas condições sejam cumpridas, o próximo "while" irá dividir "num" por "deno" até que seja encontrado um valor para "deno",
+	//maior que 1, em que o resto da divisão de "num" por "deno" seja zero, provando que "num" é "nao primo". Caso contrário, se
+	//"deno == 2" e "num" não for divisível por "deno", o programa irá assumir que "num" é "primo".
 	while (num > -1){
 		deno--;
 		if(deno != 0 && num != 1){
@@ -21,42 +29,15 @@ int main(){
 			else if (deno != 1){
 				cout << "nao primo\n";
 			}
-			else {
-				cout << "primo\n";
-			}
 			cin >> num;
 			deno = num;
 		}
+		//Isto está aqui apenas para garantir que o caso exclusivo do "num == 1" seja considerado como "nao primo", assim como solicitado
 		else{
 			cout << "nao primo\n";
 			cin >> num;
 			deno = num;
 		}
 	}
-	
-	/*
-	while (num > 0){
-		if(num > 13){
-			if (((num % 2) == 0) || ((num % 3) == 0) || ((num % 5) == 0) || ((num % 7) == 0) || ((num % 11) == 0) || ((num % 13) == 0)){
-				cout << "nao primo";
-			}
-			else {
-				cout << "primo";
-			}
-		}
-		else {
-			if (num <= 13){
-				if ((num != 2) && (num != 3) && (num != 5) && (num != 7) && (num != 11) && (num != 13)){
-					cout << "nao primo";
-				}
-				else {
-					cout  << "primo";
-				}
-			}
-		}
-	cout << "\n";
-	cin >> num;
-	}
-	*/
 	return 0;
 }
