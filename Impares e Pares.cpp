5049185e40ea5756ccc8311e5ef3397405f03f9b
Baiaopires <85589369+Baiaopires@users.noreply.github.com>
2021-09-23 23:24:29 -0300

    Update Impares e Pares.cpp

--- Impares e Pares.cpp
+++ Impares e Pares.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 
 int main(){
+    //Declaração de variáveis
 	int num, soma1, soma2, i, j;
 	
+	//Definição do valor de "num" pelo usuário
 	cin >> num;
+	
+	//Valores iniciais de j, i, soma1 e soma2
 	i = 0;
 	j = 0;
 	soma1 = 0;
 	soma2 = 0;
+	
+	//Aqui, o "while" é repetido até que o usuário digite 0
 	while (num > 0){
 		if ((num % 2) == 0){
 			++i;
@@ -22,6 +28,8 @@ int main(){
 		cin >> num;
 	}
 	
+	/*Nesta seção, o programa "printa" os respectivos valores da quantidade de impares e pares, 
+	  e a soma dos impares com impares e pares com pares*/
 	cout << j << " impares, total = " << soma2 << "\n";
 	cout << i << " pares, total = " << soma1 << "\n";
 	
